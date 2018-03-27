c897b3a90f27d2cce42446e5b7ca0c41a2cb03ac
YoavSusan <yoav.susan88@gmail.com>
2018-03-27 23:46:58 +0300

    add TankPlayerController logs

--- BattleTank/Source/BattleTank/TankPlayerController.cpp
+++ BattleTank/Source/BattleTank/TankPlayerController.cpp
@@ -2,6 +2,17 @@
 
 #include "TankPlayerController.h"
 
+void ATankPlayerController::BeginPlay() {
+	Super::BeginPlay();
+	UE_LOG(LogTemp, Warning, TEXT("PlayerController BeginPlay"));
+	ATank* ControlledTank = GetControlledTank();
+	if (ControlledTank) {
+		UE_LOG(LogTemp, Warning, TEXT("Controlled tank %s"), *ControlledTank->GetName());
+	}
+	else {
+		UE_LOG(LogTemp, Error, TEXT("PlayerController does not control a tank"));
+	}
+}
 
 ATank * ATankPlayerController::GetControlledTank() const {
 	return Cast<ATank> (GetPawn());
